c157342981566f389467e0230ef6eea20f7ca71e
Chirag Arora <carora755@gmail.com>
2021-03-12 23:19:25 +0530

    jdoodle added

--- public/script.js
+++ public/script.js
@@ -1,10 +1,17 @@
 let socket = io()
 const fun = () => {
     socket.emit('typing', {
-        text : document.getElementById('text').value
+        text : document.getElementById('code').value
+    })
+}
+
+document.getElementById('run').onclick = () => {
+    socket.emit('run', {
+        code: document.getElementById('code').value,
+        lang: 'nodejs'
     })
 }
 
 socket.on('typed', (data) => {
-    document.getElementById('text').value = data.text
+    document.getElementById('code').value = data.text
 })
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-const express = require('express')
-const app = express()
-const SERVER_PORT = process.env.PORT || 1111
-const http=  require('http')
-const server = http.createServer(app)
-const socket = require('socket.io')
-const io = socket(server)
+const express = require("express");
+const request = require("request");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-app.use('/', express.static(__dirname + '/public'))
-
-io.on('connection', (socket) => {
-    console.log(socket.id)
-    socket.on('typing', (data) => {
-        io.emit('typed', data)
-    })
-})
+app.use("/", express.static(__dirname + "/public"));
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("typing", (data) => {
+    io.emit("typed", data);
+  });
+  socket.on("run", (data) => {
+    let program = {
+      script: data.code,
+      language: data.lang,
+      versionIndex: "1",
+      clientId: "983ffeebe0cc8461ad533b8e4bc293c8",
+      clientSecret:
+        "82f737a93b6d5c7908074148cbe7cfd4e3511956a8802b8ed6b887c60c41c7f5",
+    };
+    request(
+      {
+        url: "https://api.jdoodle.com/v1/execute",
+        method: "POST",
+        json: program,
+      },
+      (error, response, body) => {
+        console.log("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        console.log("body:", body);
+      }
+    );
+  });
+});
 
 server.listen(4676, () => {
-    console.log('server started at http://localhost:4676')
-})
+  console.log("server started at http://localhost:4676");
+});
